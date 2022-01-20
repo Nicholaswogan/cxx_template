105bfc12814bfbba990738bde05fc7d1ca166a39
Nicholas Wogan <nicholas@Nicholass-MacBook-Air.local>
2022-01-20 13:42:58 -0800

    updated

--- test/test.py
+++ test/test.py
@@ -1,6 +1,14 @@
 
-from mypackage import MyClass
+
 import numpy as np
+try:
+    from mypackage import MyClass
+except:
+    import sys
+    import os
+    SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+    sys.path.append(os.path.dirname(SCRIPT_DIR))
+    from mypackage import MyClass
 
 my = MyClass()
 
